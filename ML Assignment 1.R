6d478a76641ef78bb6bf969ac9e8c47c5d237de1
Nadukula <112665602+Nadukula@users.noreply.github.com>
2022-09-12 08:08:58 +0530

    Add files via upload

---
+++ ML Assignment 1.R
@@ -0,0 +1,7 @@
+View(Rainfall_data)  
+attach(Rainfall_data)# to display the variables
+head(Rainfall_data)# to get the first five columns
+tail(Rainfall_data) # to get the last five columns
+summary(Rainfall_data)# to get median,mean and max values
+
+plot(Specific.Humidity,Temperature,main = scatterplot)# to represent the scatter plot by using plot command
